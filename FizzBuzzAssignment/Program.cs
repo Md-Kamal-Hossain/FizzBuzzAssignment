c68796a85d36194889db8a94c25dce8e7cd87376
Kamal Hossain <k.hossain@rrive.com>
2023-01-20 11:52:41 +0100

    a little formatting

--- FizzBuzzAssignment/Program.cs
+++ FizzBuzzAssignment/Program.cs
@@ -4,41 +4,36 @@ using System;
 
 namespace FizzBuzzAssignment
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
 
 
-			for (int i = 1; i <= 100; i++)
-			{
-				if (i % 3 == 0 && i % 5 != 0)
-				{
-					Console.Write("Fizz" + ",");
-				}
-				else if (i % 5 == 0 && i % 3 != 0)
-				{
-					Console.Write("Buzz" + ",");
-				}
-				else if (i % 5 == 0 && i % 3 == 0)
-				{
-					Console.Write("FizzBuzz" + ",");
-				}
-				else
-				{
-					Console.Write(i + ",");
-				}
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.Write("Fizz" + ",");
+                }
+                else if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.Write("Buzz" + ",");
+                }
+                else if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.Write("FizzBuzz" + ",");
+                }
+                else
+                {
+                    Console.Write(i + ",");
+                }
 
 
+            }
 
-			}
-
-
-
-
-
-		}
-	}
+        }
+    }
 }
 
 
